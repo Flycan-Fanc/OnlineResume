21f7316f99b0c44be8ae9b18427e27081ffd56a0
Flycan-Fanc <flycanfanc@outlook.com>
2025-05-30 19:46:57 +0800

    modified:   src/css/main.css 	modified:   src/index.html 	modified:   src/javascript/init.js 	modified:   src/javascript/main.js

--- src/javascript/init.js
+++ src/javascript/init.js
@@ -2,3 +2,20 @@
 if (!window.location.hash) {
   window.location.hash = "#home";
 }
+
+// 根据 URL hash 高亮对应菜单项
+function highlightActiveMenu() {
+  const hash = window.location.hash || "#home";
+  if (hash) {
+    const activeLink = document.querySelector(`.main-nav a[href="${hash}"]`);
+    if (activeLink) {
+      document.querySelectorAll(".main-nav a").forEach((item) => {
+        item.classList.remove("active");
+      });
+      activeLink.classList.add("active");
+    }
+  }
+}
+// 页面加载时根据当前 hash 高亮对应菜单
+// highlightActiveMenu();
+
--- src/javascript/main.js
+++ src/javascript/main.js
@@ -13,33 +13,28 @@ document.addEventListener("click", function (e) {
   }
 });
 
-// 根据 URL hash 高亮对应菜单项
-function highlightActiveMenu() {
-  const hash = window.location.hash;
-  if (hash) {
-    const activeLink = document.querySelector(`.main-nav a[href="${hash}"]`);
-    if (activeLink) {
-      document.querySelectorAll(".main-nav a").forEach((item) => {
-        item.classList.remove("active");
-      });
-      activeLink.classList.add("active");
-    }
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  // pc端菜单选择回显
-  const menuLink = document.querySelectorAll(".menu-list a");
-  menuLink.forEach((link) => {
-    link.addEventListener("click", function (e) {
-      e.preventDefault();
-      // 移除所有的active
-      menuLink.forEach((item) => item.classList.remove("active"));
-      // 给当前点击的a添加active
-      this.classList.add("active");
-      console.log(this);
-    });
+// pc端菜单选择回显
+const menuLink = document.querySelectorAll(".menu-list a");
+menuLink.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.stopPropagation();
+    // 移除所有的active
+    menuLink.forEach((item) => item.classList.remove("active"));
+    // 给当前点击的a添加active
+    this.classList.add("active");
   });
-  // 页面加载时根据当前 hash 高亮对应菜单
-  highlightActiveMenu();
 });
+
+// 页面加载时根据当前 hash 高亮对应菜单
+(function highlightActiveMenu() {
+  const hash = window.location.hash;
+  const activeLink = document.querySelector(`.main-nav a[href="${hash}"]`);
+  if (activeLink) {
+    document.querySelectorAll(".main-nav a").forEach((item) => {
+      item.classList.remove("active");
+    });
+    activeLink.classList.add("active");
+  } else {
+    throw new Error("activeLink is undefined");
+  }
+})();
