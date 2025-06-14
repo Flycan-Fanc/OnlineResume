bc4366a3dd91765547f625f5dc65d3e4655b4e0e
Flycan-Fanc <flycanfanc@outlook.com>
2025-06-14 09:01:19 +0800

    modified:   src/css/main.css 	modified:   src/css/theme.css 	modified:   src/index.html 	modified:   src/javascript/init.js 	modified:   src/javascript/main.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -118,3 +118,17 @@ menuLink.forEach((link) => {
     portfolioList.insertAdjacentHTML("beforeend", portfolioHTML);
   });
 })();
+
+(function themeSwitch() {
+  let themeSwitcher = document.querySelector(".sidebar .theme-switcher");
+
+  themeSwitcher.addEventListener("click", () => {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  });
+})();
